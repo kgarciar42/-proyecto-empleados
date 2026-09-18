27fb5b5c59b935705bfd38a89e82de4e6178a6eb
Kervin Garcia <kgarciar42@miumg.edu.gt>
2026-09-17 22:03:45 -0600

    Clase ConexionBD creada

--- proyecto-empleados-core/src/main/java/edu/umg/programacion2/proyecto/util/ConexionBD.java
+++ proyecto-empleados-core/src/main/java/edu/umg/programacion2/proyecto/util/ConexionBD.java
@@ -1,5 +1,26 @@
 package edu.umg.programacion2.proyecto.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
 
+    private static final String HOST = "localhost";
+    private static final String PUERTO = "3306";
+    private static final String BASE_DATOS = "gestion_empleados";
+
+    private static final String URL =
+            "jdbc:mysql://" + HOST + ":" + PUERTO + "/" + BASE_DATOS
+                    + "?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8";
+
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "root"; // cámbialo por tu password real
+
+    private ConexionBD() {
+    }
+
+    public static Connection obtenerConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
+    }
 }
