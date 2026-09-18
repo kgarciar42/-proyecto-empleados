99e682509ae2e3f9e340f38cc097102575931566
Kervin Garcia <kgarciar42@miumg.edu.gt>
2026-09-17 22:15:15 -0600

    Clase MainUI creada

--- proyecto-empleados-ui/src/main/java/edu/umg/programacion2/proyecto/ui/EmpleadosTableModel.java
+++ proyecto-empleados-ui/src/main/java/edu/umg/programacion2/proyecto/ui/EmpleadosTableModel.java
@@ -1,5 +1,15 @@
-package edu.umg.programacion2.proyecto.ui;
+package edu.umg.programacion2.proyecto;
 
-public class EmpleadosTableModel {
+import edu.umg.programacion2.proyecto.ui.VentanaPrincipal;
 
+import javax.swing.SwingUtilities;
+
+public class MainUI {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            VentanaPrincipal ventana = new VentanaPrincipal();
+            ventana.setVisible(true);
+        });
+    }
 }
